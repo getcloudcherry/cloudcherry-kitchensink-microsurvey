1788051eeadd56805848ee100562d4e0b3d0c0f9
Femil Shajin <femilshajin@gmail.com>
2018-08-24 17:46:13 +0530

    - Changes in Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { IntroductionComponent } from './introduction/introduction.component';
 import { PositionComponent } from './position/position.component';
@@ -50,7 +50,7 @@ const routes: Routes = [
 @NgModule( {
   imports: [
     CommonModule,
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes, { useHash: true, preloadingStrategy: PreloadAllModules } )
   ],
   exports: [ RouterModule ],
   declarations: []
